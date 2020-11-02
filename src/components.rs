09c3f60377ec17d401689c20445d7d29f0946a78
Ophois47 <dpetnick89@gamil.com>
2020-11-02 16:10:23 -0500

    Initial Commit

---
+++ src/components.rs
@@ -0,0 +1,39 @@
+use bevy::prelude::*;
+use derive_deref::*;
+
+pub struct Player {
+	pub speed: f32,
+}
+
+pub struct Weapon {
+	pub fired: bool,
+	pub offset: Vec3,
+	pub cooldown: Timer,
+	pub material_id: usize,
+}
+
+pub struct Laser {
+	pub speed: f32,
+}
+
+pub struct Environment;
+pub struct Enemy;
+pub struct Bounded;
+pub struct LoseHealthOnCollide;
+pub struct CollisionDamage(pub i32);
+pub struct Health(pub i32);
+
+pub enum DeathBehavior {
+	Despawn,
+	None,
+}
+
+pub enum CollisionLayer {
+	Player,
+	Enemies,
+	PlayerProjectiles,
+	EnemyProjectiles,
+}
+
+#[derive(Deref)]
+pub struct CollidesWith(Vec<CollisionLayer>);
